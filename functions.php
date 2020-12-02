f777c48891c61c66270657ec16a87089da055e26
w@rren <w@rren.dev>
2020-12-02 12:23:58 +0000

    trimmed unnecessary comment functions

--- functions.php
+++ functions.php
@@ -1,10 +1,7 @@
 <?php
-/**
- * Set up theme defaults and registers support for various WordPress feaures.
- */
+
 add_action( 'after_setup_theme', function() {
 	load_theme_textdomain( 'bathe', get_theme_file_uri( 'languages' ) );
-
 	add_theme_support( 'automatic-feed-links' );
 	add_theme_support( 'title-tag' );
 	add_theme_support( 'post-thumbnails' );
@@ -25,11 +22,8 @@ add_action( 'after_setup_theme', function() {
 } );
 
 add_action( 'wp_enqueue_scripts', function() {
-
 	wp_enqueue_style( 'bathe-main', get_theme_file_uri( 'assets/css/main.css' ) );
-
 	wp_enqueue_script( 'bathe-bundle', get_theme_file_uri( 'assets/js/bundle.js' ), array(), null, true );
-
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
@@ -60,70 +54,14 @@ function pluralize($quantity, $singular, $plural=null) {
 	}
 }
 
-function twentynineteen_comment_form_defaults( $defaults ) {
-	$comment_field = $defaults['comment_field'];
-
-	// Adjust height of comment form.
-	$defaults['comment_field'] = preg_replace( '/rows="\d+"/', 'rows="5"', $comment_field );
-
-	return $defaults;
-}
-add_filter( 'comment_form_defaults', 'twentynineteen_comment_form_defaults' );
-
-/**
- * Returns information about the current post's discussion, with cache support.
- */
-function twentynineteen_get_discussion_data() {
-	static $discussion, $post_id;
-
-	$current_post_id = get_the_ID();
-	if ( $current_post_id === $post_id ) {
-		return $discussion; /* If we have discussion information for post ID, return cached object */
-	} else {
-		$post_id = $current_post_id;
-	}
-
-	$comments = get_comments(
-		array(
-			'post_id' => $current_post_id,
-			'orderby' => 'comment_date_gmt',
-			'order'   => get_option( 'comment_order', 'asc' ), /* Respect comment order from Settings » Discussion. */
-			'status'  => 'approve',
-		)
-	);
-
-	$authors = array();
-	foreach ( $comments as $comment ) {
-		$authors[] = ( (int) $comment->user_id > 0 ) ? (int) $comment->user_id : $comment->comment_author_email;
-	}
-
-	$authors    = array_unique( $authors );
-	$discussion = (object) array(
-		'authors'   => array_slice( $authors, 0, 6 ),           /* Six unique authors commenting on the post. */
-		'responses' => get_comments_number( $current_post_id ), /* Number of responses. */
-	);
-
-	return $discussion;
-}
-
-// This will occur when the comment is posted
 function plc_comment_post( $incoming_comment ) {
-
-	// convert everything in a comment to display literally
 	$incoming_comment['comment_content'] = htmlspecialchars($incoming_comment['comment_content']);
-
-	// the one exception is single quotes, which cannot be #039; because WordPress marks it as spam
 	$incoming_comment['comment_content'] = str_replace( "'", '&apos;', $incoming_comment['comment_content'] );
-
 	return( $incoming_comment );
 }
 
-// This will occur before a comment is displayed
 function plc_comment_display( $comment_to_display ) {
-
-	// Put the single quotes back in
 	$comment_to_display = str_replace( '&apos;', "'", $comment_to_display );
-
 	return $comment_to_display;
 }
 
