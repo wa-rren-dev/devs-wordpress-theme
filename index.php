733f973c08848f6ba7394b8898d9d3fdd781d367
w@rren <w@rren.dev>
2021-02-18 15:09:31 +0000

    removed head and foot

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
 	<meta charset="<?php bloginfo('charset'); ?>"/>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
@@ -61,8 +60,6 @@
 		</main>
 	</div>
 <?php } ?>
-
-<?php wp_footer(); ?>
 
 </body>
 </html>
